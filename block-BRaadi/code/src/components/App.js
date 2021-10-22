7b0cb06d1875c11c71b19250c05afc444f6352f1
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-10-23 02:05:12 +0530

    error boundary

--- block-BRaadi/code/src/components/App.js
+++ block-BRaadi/code/src/components/App.js
@@ -1,14 +1,16 @@
-import React from "react";
-import Header from "./Header";
-import Main from "./Main";
-
+import React from 'react';
+import Header from './Header';
+import Main from './Main';
+import ErrorBoundary from './ErrorBoundary';
 class App extends React.Component {
   render() {
     let { isDarkMode } = this.state;
     return (
       <>
-        <Header isDarkMode={isDarkMode} />
-        <Main isDarkMode={isDarkMode} />
+        <ErrorBoundary>
+          <Header isDarkMode={isDarkMode} />
+          <Main isDarkMode={isDarkMode} />
+        </ErrorBoundary>
       </>
     );
   }
--- block-BRaadi/code/src/index.js
+++ block-BRaadi/code/src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./style/app.css";
-import App from "./components/App";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/app.css';
+import App from './components/App';
+import ErrorBoundary2 from './components/ErrorBoundary2';
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary2>
+      <App />
+    </ErrorBoundary2>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
